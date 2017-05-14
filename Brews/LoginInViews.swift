f2df5239710a93561fde8b91c8221fdcf6984266
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2017-05-14 11:02:48 -0500

    Add Login Button Constraints

---
+++ Brews/LoginInViews.swift
@@ -0,0 +1,10 @@
+//
+//  LoginInViews.swift
+//  Brews
+//
+//  Created by Michael Doroff on 5/14/17.
+//  Copyright © 2017 Michael Doroff. All rights reserved.
+//
+
+import Foundation
+import UIKit
--- Brews/ViewController.swift
+++ Brews/ViewController.swift
@@ -11,7 +11,7 @@ import FBSDKLoginKit
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
    
     
@@ -20,13 +20,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-    
+        
     let loginButton = FBSDKLoginButton()
+    loginButton.translatesAutoresizingMaskIntoConstraints = false
     view.addSubview(loginButton)
-    loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+    loginButton.frame = CGRect(x: view.frame.width / 2, y: view.frame.height / 2, width: view.frame.width, height: 50)
+    loginButton.delegate = self
+    loginButton.readPermissions = ["email", "public_profile"]
     
+        
+        let horizontalConstraint = loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let verticalConstraint = loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        let widthConstraint = loginButton.widthAnchor.constraint(equalToConstant: view.frame.width)
+        let heightConstraint = loginButton.heightAnchor.constraint(equalToConstant: 100)
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+
+        
+        showEmailAddress()
+        
+        
+    }
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of Facebook...")
     }
 
+    func showEmailAddress() {
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start {(connection, result, err) in
+            
+            
+            if err != nil {
+                print("Failed to start graph request")
+                return
+            }
+            
+            
+            
+            
+        }
+        
+    }
 
 
 }
