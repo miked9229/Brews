fb3ca21c5dba379608201d53b2f1bad179f92e0a
Michael Doroff <michael_doroff@yahoo.com>
2017-06-23 18:20:49 -0500

    Fixed BackBarButton Item on SelectedBeerView

--- Brews/LogInViewController.swift
+++ Brews/LogInViewController.swift
@@ -25,7 +25,6 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSignIn
         setUpBackGroundImage()
         listenForUserAuthentication()
   
-
     }
     
 
--- Brews/SelectedBeerViewController.swift
+++ Brews/SelectedBeerViewController.swift
@@ -35,6 +35,7 @@ class SelectedBeerViewController: UIViewController {
         downloadImage()
        listenForUserAuthentication()
        disableFavoritesButton()
+      self.navigationController?.navigationBar.topItem?.title = ""
     
        
         
